5015f6e956ec461d072a6b07aeb0d919fca266bd
lynakimhay <nakimhay.ly@institute.pse.ngo>
2024-06-06 11:07:58 +0700

    hai

--- dom1/index.js
+++ dom1/index.js
@@ -2,3 +2,7 @@ const article = {
   title: "How to code",
   description: "You can learn to code. Error is nothing to be afraid of.",
 };
+document.getElementById('displayButton').addEventListener('click',function(){
+  const articleDiv = document.getElementById('article');
+  articleDiv.innerHTML = `<h2>${article.title}</h2><p>${article.description}</p>`
+});
